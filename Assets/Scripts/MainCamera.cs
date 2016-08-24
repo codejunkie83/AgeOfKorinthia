434a0855085d7e628a09bb3aca0f2ab0335ab84e
codejunkie83 <codejunkie83@live.com>
2016-08-23 23:20:30 -0500

    Camera/Scene
    Added basic camera movement, as well as click-to-move and navigation for
    PC.

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class MainCamera : MonoBehaviour {
 
+    public float HorizontalCameraSpeed;
+    public float VerticalCameraSpeed;
+    public float ZoomSpeed;
+
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(HorizontalCameraSpeed * Time.deltaTime, 0.0f, 0.0f));
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-HorizontalCameraSpeed * Time.deltaTime, 0.0f, 0.0f));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0.0f, 0.0f, VerticalCameraSpeed * Time.deltaTime), Space.World);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0.0f, 0.0f, -VerticalCameraSpeed * Time.deltaTime), Space.World);
+        }
 	}
 }
